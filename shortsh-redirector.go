91893e058edd49cd592403838c29ef3f088115ed
Richard Szolár <r@richardszolar.me>
2018-11-03 18:11:23 +0100

    - Initial commit

---
+++ shortsh-redirector.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"github.com/0x111/shortsh-redirector/models"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/core"
+	"github.com/go-xorm/xorm"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/spf13/viper"
+	"log"
+	"net/http"
+	"runtime"
+)
+
+var engine *xorm.Engine
+
+func main() {
+	// maximize CPU usage for maximum performance
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	var err error
+	// read config
+	viper.SetConfigName("config")    // name of config file (without extension)
+	viper.AddConfigPath("./_config") // optionally look for config in the working directory
+	viper.AddConfigPath(".")         // optionally look for config in the working directory
+	err = viper.ReadInConfig()       // Find and read the config file
+	if err != nil {                  // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
+	engine, err = xorm.NewEngine("mysql", viper.GetString("mysql_dsn"))
+
+	if err != nil {
+		log.Fatalf("We could not connect to the database %v\n", err)
+	}
+
+	engine.ShowSQL(true)
+	engine.Logger().SetLevel(core.LOG_DEBUG)
+
+	// Sync models
+	engine.Sync2(new(models.ShortShUrl))
+
+	e := echo.New()
+
+	e.Use(middleware.RequestIDWithConfig(middleware.RequestIDConfig{}))
+
+	e.GET("/:shortId", func(c echo.Context) error {
+		shortID := c.Param("shortId")
+		var shortURL = models.ShortShUrl{ShortId: shortID}
+		has, err := engine.Get(&shortURL)
+		fmt.Println(has, err, shortURL)
+		if has && err == nil {
+			return c.Redirect(http.StatusMovedPermanently, shortURL.Url)
+		}
+
+		return c.Redirect(http.StatusMovedPermanently, "https://short.sh")
+	})
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Redirect(http.StatusMovedPermanently, "https://short.sh")
+	})
+
+	e.Logger.Fatal(e.Start(":1324"))
+}
